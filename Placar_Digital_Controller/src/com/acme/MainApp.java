3c822b4a67c719d3797fb30b839dbfa76af12592
MateusPDK <mateus-pk@outlook.com>
2018-05-06 19:57:22 -0300

    Telas de Juiz/Placar

--- Placar_Digital_Controller/src/com/acme/MainApp.java
+++ Placar_Digital_Controller/src/com/acme/MainApp.java
@@ -7,10 +7,13 @@ package com.acme;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 /**
  *
@@ -20,12 +23,27 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent fxmlCena = FXMLLoader.load(this.getClass().getResource("/com/acme/view/LoginScreenController.fxml"));
-        Scene cena = new Scene(fxmlCena);
+        Parent fxmlCena = FXMLLoader.load(this.getClass().getResource("/com/acme/view/Juiz.fxml"));
+        StackPane root = new StackPane();
+        Rectangle2D r = Screen.getPrimary().getBounds();
+        Scene cena = new Scene(fxmlCena, r.getWidth(), r.getHeight());
+
+        System.out.println("x: "+r.getWidth()+" y: "+r.getHeight());
+
+        Rectangle rect = new Rectangle(r.getWidth(), r.getHeight());
+        root.getChildren().add(rect);
+
+        // scene.setCursor(Cursor.NONE);  // Uncomment, if you don't need a cursor
         primaryStage.setScene(cena);
-        primaryStage.setResizable(false);
-        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setFullScreen(true);
         primaryStage.show();
+//        Parent fxmlCena = FXMLLoader.load(this.getClass().getResource("/com/acme/view/Placar.fxml"));
+//        Scene cena = new Scene(fxmlCena);
+//        primaryStage.setScene(cena);
+//        primaryStage.setResizable(false);
+//        primaryStage.initStyle(StageStyle.UNDECORATED);
+//        primaryStage.setFullScreen(true);
+//        primaryStage.show();
     }
 
     public static void main(String[] args) {
--- Placar_Digital_Controller/src/com/acme/controller/FXMLDocumentController.java
+++ Placar_Digital_Controller/src/com/acme/controller/FXMLDocumentController.java
@@ -1,11 +1,20 @@
 package com.acme.controller;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class FXMLDocumentController {
 
     @FXML
-    private JFXButton btnSingIn;
+    private Button button;
+    @FXML
+    private Label label;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+    }
 
 }
